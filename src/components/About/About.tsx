df0d0b2ffbce1f0b346772457e68ac4ce65c9a6d
LhunaCat <viktorijamartinaityte@gmail.com>
2023-02-15 13:24:42 +0200

    added about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,14 +1,24 @@
+import "./About.css";
+
 const About = () => {
+  const currentDate = new Date();
+  const startDate = new Date("2022-05-01");
+
+  const dateDiff =
+    currentDate.getMonth() -
+    startDate.getMonth() +
+    12 * (currentDate.getFullYear() - startDate.getFullYear());
+
   return (
-    <div className="text">
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis
-        inventore laboriosam ad rem at, nulla voluptas explicabo quaerat vel
-        quisquam aperiam corporis asperiores nemo possimus consectetur soluta in
-        eum cupiditate aliquid fugit, magnam cumque facere iste necessitatibus.
-        A, exercitationem inventore, accusamus maxime minus repellat praesentium
-        autem nulla iusto dolor dignissimos!
+    <div className="about">
+      <h2 className="about__title">I'm Viktorija</h2>
+      <p className="about__text">
+        I'm a junior{" "}
+        <span className="about__text--underlined">Software Developer</span> who
+        started this coders journey {dateDiff} months ago. I was always curious
+        about how web pages or games came to life and was beyond excited when
+        found CodeAcademy Full Stack course. My current favourite aesthetic is
+        cyberpunk and I recently started drawing.
       </p>
     </div>
   );
