0c83150eb746ba5162d6e23a9c34ade707c9ec83
LhunaCat <viktorijamartinaityte@gmail.com>
2023-02-16 14:57:37 +0200

    added resume and changed css

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faLinkedin, faSquareGithub } from "@fortawesome/free-brands-svg-icons";
+
+// import resume from '../../assets/resume.pdf';
 
 import "./Header.css";
+
+library.add(faLinkedin, faSquareGithub);
 
 const Header = () => {
   return (
@@ -9,6 +16,35 @@ const Header = () => {
         <Link to="/" className="logo__text">
           VM
         </Link>
+      </div>
+      <div className="socials">
+        <a
+          href="https://www.linkedin.com/in/viktorija-martinaityte-softdev/"
+          className="socials__link"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={["fab", "linkedin"]}
+            className="socials__icon"
+          />
+        </a>
+        <a
+          href="https://github.com/ViktorijaMart"
+          className="socials__link"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={["fab", "square-github"]}
+            className="socials__icon"
+          />
+        </a>
+        <a
+          href={require("../../assets/resume.pdf")}
+          className="socials__link"
+          target="_blank"
+        >
+          <div className="socials__link--custom">Resume</div>
+        </a>
       </div>
     </div>
   );
