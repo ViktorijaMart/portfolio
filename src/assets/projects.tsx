1a635422be3c97861d2daa53f1075e58458c1080
LhunaCat <viktorijamartinaityte@gmail.com>
2023-02-16 18:42:41 +0200

    mock projects data

---
+++ src/assets/projects.tsx
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    id: 1,
+    name: "Project 1",
+    categorie: "frontend",
+    description: "This is description",
+    languages: ["HTML", "CSS", "Javascript"],
+    github: "https://github.com/ViktorijaMart",
+    page: "https://github.com/ViktorijaMart",
+  },
+  {
+    id: 2,
+    name: "Project 2",
+    categorie: "frontend",
+    description: "This is description",
+    languages: ["HTML", "CSS", "Javascript"],
+    github: "https://github.com/ViktorijaMart",
+    page: "https://github.com/ViktorijaMart",
+  },
+  {
+    id: 3,
+    name: "Project 3",
+    categorie: "backend",
+    description: "This is description",
+    languages: ["HTML", "CSS", "PHP"],
+    github: "https://github.com/ViktorijaMart",
+    page: "https://github.com/ViktorijaMart",
+  },
+];
