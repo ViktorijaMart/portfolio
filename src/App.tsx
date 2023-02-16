b96a79177905e3bea2f91ff5dec14501df0de704
LhunaCat <viktorijamartinaityte@gmail.com>
2023-02-16 18:44:37 +0200

    added Project route

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Navigation from "./components/Layout/Navigation";
 import NotFound from "./components/NotFound/NotFound";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
+import { projects } from "./assets/projects";
+import Project from "./components/Projects/Project/Project";
 
 function App() {
   return (
@@ -18,14 +20,23 @@ function App() {
           <div className="row">
             <Navigation />
             <div className="hologram-container">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/skills" element={<Skills />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
+              <div className="hologram-text">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/skills" element={<Skills />} />
+                  <Route
+                    path="/projects"
+                    element={<Projects projects={projects} />}
+                  />
+                  <Route
+                    path="/projects/:id"
+                    element={<Project projects={projects} />}
+                  />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </div>
               <div className="holographic-effect"></div>
             </div>
           </div>
